2ce86a0ce3d15d0085d82a95cd13f8476fcbc941
Will2430 <william233224@gmail.com>
2026-07-08 16:38:57 +0800

    Add S3-compatible object storage support
    - Experiment with nginx config with s3 compatible object storage, e.g. Rustfs, Minio
    - Refactor initial s3 logic

--- mvp/src/storage/s3.js
+++ mvp/src/storage/s3.js
@@ -3,7 +3,18 @@ const { config } = require("../config");
 
 AWS.config.update({ region: config.awsRegion });
 
-const s3 = new AWS.S3();
+const s3InitOptions = {
+    signatureVersion: "u4"
+};
+
+if (config.s3Endpoint) {
+    s3InitOptions.endpoint = config.s3Endpoint;
+    s3InitOptions.s3ForcePathStyle = config.s3ForcePathStyle || false; // Required for custom endpoint
+} 
+
+const s3 = new AWS.S3(
+    config.s3Endpoint ? s3InitOptions : undefined
+);
 
 async function uploadPdf({ key, buffer, contentType }) {
   return s3
